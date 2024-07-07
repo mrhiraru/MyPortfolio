c88010f17792b152dab638731feb812812a7d6ca
mrhiraru <mrhiraru@gmail.com>
2024-07-07 16:14:17 +0800

    2

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     });
 });
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.nav-link');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('.nav-link[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
